e409f5dd060c738ac7246d9fb127baf0a7c23fe5
Annamalai <Annamalai@course5i.com>
2023-07-25 20:37:10 +0530

    first commit with databricks SDK

---
+++ app/app.py
@@ -0,0 +1,57 @@
+
+from fastapi import FastAPI
+#from mangum import Mangum
+from fastapi.responses import JSONResponse
+import uvicorn
+from mlflow.pyfunc import load_model
+import mlflow
+from databricks.sdk import WorkspaceClient
+from mlflow import MlflowClient
+import numpy as np
+import os
+
+
+os.environ['DATABRICKS_HOST'] = "https://dbc-ecf08c36-d508.cloud.databricks.com/"
+os.environ['DATABRICKS_TOKEN'] = "dapif21d8059c1d128d1ef3f836b7f8799db"
+
+w = WorkspaceClient(
+  host  = os.environ.get('DATABRICKS_HOST'),
+  token = os.environ.get('DATABRICKS_TOKEN')
+)
+
+mlflow.set_tracking_uri('databricks')
+
+for mv in w.model_registry.search_model_versions(filter="name='sk-learn-logistic-reg-model'"):
+                                          #dic = dict(mv)  
+                                          dic = mv.__dict__
+                                          #print(dic['run_id'])
+                                          if dic['current_stage']=='Production':
+                                                   d = w.experiments.get_run(run_id=dic['run_id']).run
+                                                   #print(d.data.metrics) ---------->metrics
+                                                   print(dic['source'])
+                                                   model_uri = dic['source']
+                                                   my_model = load_model(model_uri)
+
+app = FastAPI()
+#handler = Mangum(app)
+
+@app.get("/")
+def read_root():
+   return {"Welcome to": "My first FastAPI depolyment using Docker image"}
+
+@app.get("/{text}")
+def read_item(text: str):
+   return JSONResponse({"result": text})
+
+
+
+@app.get("/invocations/{arr}")
+def predict(arr: list):
+                 ans = my_model.predict(np.array(arr).reshape(1,4))
+
+                 return {"status":200, "prediction":ans}
+       
+
+if __name__ == "__main__":
+   uvicorn.run(app, host="0.0.0.0", port=8080)
+
